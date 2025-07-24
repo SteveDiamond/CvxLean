5fe148c639e26aa34df3a6fd2995f81cebe141c2
Steven Diamond <steven@gridmatic.com>
2025-07-23 22:07:20 -0700

    remove integration status

--- CvxLean/Command/Solve/Mosek/Path.lean
+++ CvxLean/Command/Solve/Mosek/Path.lean
@@ -1,4 +1,4 @@
 /-! Change this to the location of the Mosek binary, e.g.
 `/Users/alice/mosek/10.0/tools/platform/osxaarch64/bin` -/
 
-def mosekBinPath := ""
+def mosekBinPath := "/Users/sdiamond/mosek/10.0/tools/platform/osxaarch64/bin"
